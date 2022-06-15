fa0d9820f33bf0adf9235742e0a6863c025a3d77
Fauzer Junior <fauzer.junior.ext@dasa.com.br>
2022-06-15 18:58:02 -0300

    feat: add categoria crud methods

--- application/classes/Categoria.js
+++ application/classes/Categoria.js
@@ -1,8 +1,92 @@
-import { mockListaCategoria } from '../mocks'
+import { mockListaCategoria } from "../mocks";
 
 export class Categoria {
   constructor() {
     this.listaCategoria = mockListaCategoria;
+  }
+
+  get() {
+    new Promise((resolve, reject) => {
+      if (!this.listaCategoria) {
+        return setTimeout(
+          () =>
+            reject(
+              new Error("Não foi possível carregar a lista de categorias")
+            ),
+          250
+        );
+      }
+
+      setTimeout(() => resolve(Object.values(this.listaCategoria)), 250);
+    });
+  }
+
+  getById(id) {
+    new Promise((resolve, reject) => {
+      const categoria = this.listaCategoria[id];
+
+      if (!categoria) {
+        return setTimeout(
+          () =>
+            reject(
+              new Error(`Não foi possível carregar a categoria de id ${id}`)
+            ),
+          250
+        );
+      }
+
+      setTimeout(() => resolve(this.listaCategoria[id]), 250);
+    });
+  }
+
+  post(data) {
+    new Promise((resolve, reject) => {
+      if (!data) {
+        reject(new Error("Não foi possível cadastrar a categoria"));
+      }
+
+      const id = Object.keys(this.listaCategoria).length + 1;
+      const novaCategoria = {
+        id,
+        ...data,
+      };
+
+      this.listaCategoria = { ...this.listaCategoria, [id]: novaCategoria };
+      setTimeout(() => resolve(data), 250);
+    });
+  }
+
+  update(id, data) {
+    new Promise((resolve, reject) => {
+      if (!this.listaCategoria[id]) {
+        return setTimeout(
+          () =>
+            reject(
+              new Error(`Não foi possível atualizar a categoria de id ${id}`)
+            ),
+          250
+        );
+      }
+
+      users[id] = { ...users[id], ...data };
+      return setTimeout(() => resolve(true), 250);
+    });
+  }
+
+  delete(id) {
+    new Promise((resolve, reject) => {
+      const { [id]: user, ...rest } = this.listaCategoria;
+
+      if (!user) {
+        return setTimeout(
+          () => reject(new Error("Não foi possível deletar a categoria")),
+          250
+        );
+      }
+
+      this.listaCategoria = { ...rest };
+      return setTimeout(() => resolve(true), 250);
+    });
   }
 
   consultarCategoria() {
